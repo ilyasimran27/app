c10b67ff256c77e95cf8289f187507defe2ed3ad
ilyasimran27 <50030345+ilyasimran27@users.noreply.github.com>
2019-06-16 05:03:47 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,146 +1,114 @@
-function apple_plus() {
-    var apple = 10;
-    var app = document.querySelector("#app-price").value;
-    if (app < apple) {
-        app++;
-        document.querySelector("#app-price").value = app;
-        priceapp(app);
+var total_price=0;
+var count_apple=0;
+var count_orange=0;
+var count_ban=0;
+var purchase=0;
+var total_purchase=0;
+function total_amount(){
+    total_price=document.querySelector("#put-money").value;
+}
+function purchasing(){
+    document.querySelector("span").innerHTML = total_purchase;
+}
+function apple_plus(){
+    if(total_price >= 10){
+        count_apple++;
+       
+        purchase+=10;
+        total_price=total_price-purchase;
+        total_purchase=total_purchase+purchase;
+        purchase=0;
+        document.querySelector("#app-price").value=count_apple;
+        purchasing();
 
     }
-
-
-
-}
-
-function apple_minus() {
-    var apple = 10;
-    var app = document.querySelector("#app-price").value;
-    if (app <= apple && app > 0) {
-        app--;
-        document.querySelector("#app-price").value = app;
-        priceapp(app);
-
-    }
-
-
-
-}
-
-function orange_plus() {
-    var orange = 15;
-    var org = document.querySelector("#org-price").value;
-    if (org < orange) {
-        org++;
-        document.querySelector("#org-price").value = org;
-        priceorg(org);
-
-
-    }
-
-
-
-}
-
-function orange_minus() {
-    var orange = 15;
-    var org = document.querySelector("#org-price").value;
-    if (org <= orange && org > 0) {
-        org--;
-        document.querySelector("#org-price").value = org;
-        priceorg(org);
-
-    }
-
-
-
-}
-
-function ban_plus() {
-    var banana = 7;
-    var ban = document.querySelector("#ban-price").value;
-    if (ban < banana) {
-        ban++;
-        document.querySelector("#ban-price").value = ban;
-        priceban(ban);
-    }
-
-
-
-}
-
-function ban_minus() {
-    var banana = 7;
-    var ban = document.querySelector("#ban-price").value;
-    if (ban <= banana && ban > 0) {
-        ban--;
-        document.querySelector("#ban-price").value = ban;
-
-        priceban(ban);
-    }
-
-
-
-}
-var sum = 0;
-var price1 = 0;
-
-function priceapp(a) {
-    var putmoney = document.querySelector("#put-money").value;
-    price1 = a * 10;
-
-
-    sum = price2 + price1 + price3;
-
-
-
-    if (sum <= putmoney) {
-
-
-        document.querySelector("#owner").value = sum;
-    } else {
-        alert("sorry you do not have money");
+    else{
+        alert("sorry you do not have maoney to buy apple");
     }
 
 }
-var sum2 = 0;
-var price2 = 0;
+function apple_minus(){
+    if(total_purchase > 0 && count_apple > 0){
+        count_apple--;
+       
+        purchase+=10;
+        total_price=total_price+purchase;
+        total_purchase=total_purchase-purchase;
+        purchase=0;
+        document.querySelector("#app-price").value=count_apple;
+        purchasing();
 
-function priceorg(o) {
-    var putmoney = document.querySelector("#put-money").value;
-    price2 = o * 15;
-
-
-    sum2 = price1 + price2 + price3;
-
-
-
-    if (sum2 <= putmoney) {
-
-
-        document.querySelector("#owner").value = sum2;
-    } else {
-        alert("sorry you do not have money");
+    }
+    else{
+        alert("sorry you do not have maoney to buy apple");
     }
 
 }
-var sum3 = 0;
-var price3 = 0;
+function orange_plus(){
+    if(total_price >= 15){
+        count_orange++;
+       
+        purchase+=15;
+        total_price=total_price-purchase;
+        total_purchase=total_purchase+purchase;
+        purchase=0;
+        document.querySelector("#org-price").value=count_orange;
+        purchasing();
 
-function priceban(b) {
-    var putmoney = document.querySelector("#put-money").value;
-    price3 = b * 7;
+    }
+    else{
+        alert("sorry you do not have maoney to buy orange");
+    }
 
+}
+function orange_minus(){
+    if(total_purchase > 0 && count_apple > 0){
+        count_orange--;
+       
+        purchase+=15;
+        total_price=total_price+purchase;
+        total_purchase=total_purchase-purchase;
+        purchase=0;
+        document.querySelector("#org-price").value=count_orange;
+        purchasing();
 
-    sum3 = price2 + price1 + price3;
+    }
+    else{
+        alert("sorry you do not have maoney to buy orange");
+    }
 
+}
+function ban_plus(){
+    if(total_price >= 7){
+        count_ban++;
+       
+        purchase+=7;
+        total_price=total_price-purchase;
+        total_purchase=total_purchase+purchase;
+        purchase=0;
+        document.querySelector("#ban-price").value=count_ban;
+        purchasing();
 
+    }
+    else{
+        alert("sorry you do not have maoney to buy banana");
+    }
 
-    if (sum3 <= putmoney) {
+}
+function ban_minus(){
+    if(total_purchase > 0 && count_apple > 0){
+        count_ban--;
+       
+        purchase+=7;
+        total_price=total_price+purchase;
+        total_purchase=total_purchase-purchase;
+        purchase=0;
+        document.querySelector("#ban-price").value=count_ban;
+        purchasing();
 
-
-        document.querySelector("#owner").value = sum3;
-    } else {
-        alert("sorry you do not have money");
+    }
+    else{
+        alert("sorry you do not have maoney to buy banana");
     }
 
 }
